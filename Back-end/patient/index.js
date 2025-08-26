6289bcf101ecbcf580fe630d8ec695f91befb939
Gyan <148750194+Markgian06@users.noreply.github.com>
2025-08-26 16:23:11 +0800

    paloddddd
    sds

--- Back-end/patient/index.js
+++ Back-end/patient/index.js
@@ -3,10 +3,13 @@ import express from "express";
 import cors from "cors";
 import dbConnection from './Controllers/dbConnection.js';
 
+
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({credentials: true}));
 app.use(express.json());
+
+
 
 app.listen(port, () => {
     dbConnection();
