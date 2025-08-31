b610870f7c7272949637463f954c848b565aeadf
Gyan <148750194+Markgian06@users.noreply.github.com>
2025-08-31 12:00:26 +0800

    update ko lmh

--- Back-end/patient/Js/index.js
+++ Back-end/patient/Js/index.js
@@ -5,10 +5,10 @@
     const authLink = document.getElementById("authLink");
 
      if (isLoggedIn) {
-        scheduleOrAppointment.innerHTML = `<a href="frontend/patient/html/appoinment.html" class="nav-link">Appointment</a>`;
-        authLink.innerHTML = `<a href="frontend/patient/html/profile.html" class="nav-link">Profile</a>`;
+        scheduleOrAppointment.innerHTML = `<a href="/frontend/patient/html/appoinment.html" class="nav-link">Appointment</a>`;
+        authLink.innerHTML = `<a href="/frontend/patient/html/profile.html" class="nav-link">Profile</a>`;
       } else {
         scheduleOrAppointment.innerHTML = `<a href="/frontend/patient/html/schedule.html" class="nav-link">Schedule</a>`;
-        authLink.innerHTML = `<a href="frontend/patient/html/login.html" class="nav-link">Login</a>`;
+        authLink.innerHTML = `<a href="/frontend/patient/html/login.html" class="nav-link">Login</a>`;
       }
     });
