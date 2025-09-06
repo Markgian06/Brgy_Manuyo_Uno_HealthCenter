54fb02ea67b34b05fa85ec500aaf788962cfc77c
Gyan <148750194+Markgian06@users.noreply.github.com>
2025-09-06 10:11:54 +0800

    palo

--- Back-end/patient/Routes/appointmentRoutes.js
+++ Back-end/patient/Routes/appointmentRoutes.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { createAppointment } from "../Controllers/appointmentControllers.js";
 import { validateAppointment } from "../Middleware/appointmentMiddleware.js";
-import { getAppointmentsByDate } from "../Controllers/getAppointmentsByDate.js";
 
 const router = express.Router();
 
-router.get("/appointments/date/:date", getAppointmentsByDate);
 router.post("/appointments", validateAppointment, createAppointment);
 
 
